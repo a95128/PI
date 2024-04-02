c83ebcc10245163cfd30e5ee89dec5c29a78a5be
Ana João Alves <a95128@alunos.uminho.pt>
2024-04-02 20:04:23 +0100

    Add files via upload

--- FICHAS/ficha5pi.c
+++ FICHAS/ficha5pi.c
@@ -9,6 +9,19 @@ typedef struct aluno {
     float teste;
 } Aluno;
 
+void swap(int v[], int j, int i) {
+    int aux;
+    aux = v[j];
+    v[j] = v[i];
+    v[i] = aux;
+}
+
+void swap_aluno(Aluno t[], int i,int j) {
+    Aluno tmp = t[i];
+    t[i] = t[j];
+    t[j] = tmp;
+}
+
 //EXERCICIO 1
 
 int nota (Aluno a) {
@@ -20,7 +33,7 @@ int nota (Aluno a) {
 
     float nota_final = media_mini * 0.2 + a.teste * 0.8;
 
-    int nota = roundf(nota_final);
+    int nota = (nota_final);
     if(nota < 10) return 0;
     else { return nota;}
 }
@@ -38,24 +51,55 @@ int procuraNum (int num, Aluno t[], int N) {
 
 //EXERCICIO 3
 
-void swap_aluno(t,i,j) {
-    Aluno tmp = t[i];
-    t[i] = t[j];
-    t[j] = tmp;
-}
 
 void ordenaPorNum(Aluno t[], int N) {
     int i, j;
     for(i = 0; i < N; i++) {
         for(j = 0; j < N-1; j++) {
-            if(t[j].num > t[j+1].num) {
+            if(t[j].numero > t[j+1].numero) {
                 swap_aluno(t, j, j+1);
             }
         }
     }
 }
 
+//EXERCICIO 4
+
+void criaIndPorNum4(Aluno t[], int N, int ind[]) {
+    int count;
+    for(int i = 0; i < N ; i++) {
+        count = 0;
+        for(int j = 0; j < N; j++) {
+            if(t[j].numero > t[i].numero) {
+                count++;
+            }
+        }
+        ind[i] = count;
+    }
+
+}
+
+//EXERCICIO 5
+
+void imprimeTumra(int ind[], Aluno t[], int N) {
+    Aluno aux[N];
+    for(int i=0; i < N; i++) {
+        aux[i] = t[ind[i]];
+    }
+    for(int i=0; i < N; i++) {
+        printf("%d ", aux[i].numero);
+        printf("%s ", aux[i].nome);
+        for(int j =0; j < 6; j++) {
+            printf("%d ", aux[i].miniT[j]);
+        }
+        printf("%f ", aux[i].teste);
+        printf("\n");
+    }
+
+}
+
 //EXERCICIO 6
+
 
 void criaIndPorNum (Aluno t [], int N, int ind[]) {
     int i , j;
@@ -70,7 +114,7 @@ void criaIndPorNum (Aluno t [], int N, int ind[]) {
 
 //EXERCICIO 7
 
-void criaIndPorNum (Aluno t [], int N, int ind[]) {
+void criaIndPorNome (Aluno t [], int N, int ind[]) {
     int i , j;
     for(int i = 0; i < N ; i++) {
         for(int j = 0; j < N-i-1; j++ ) {
@@ -95,7 +139,7 @@ int main() {
     aluno1.miniT[5] = 92;
     aluno1.teste = 85.5;
     int c = nota(aluno1);
-    printf("%d",c);
+    //printf("%d\n",c);
     Aluno aluno2;
 
     aluno2.numero = 2;
@@ -122,7 +166,18 @@ int main() {
     
     
     Aluno numeros[3] = {aluno1, aluno2, aluno3};
-    int res = procuraNum(2, numeros, 3);
-    printf("%d", res); 
+    //int res = procuraNum(2, numeros, 3);
+    //printf("%d\n", res); 
+
+    Aluno array[] = 
+    {{4444, "Andre", {2,1,0,2,2,2}, 10.5}
+    ,{2222, "Joana", {2,2,2,1,0,0}, 14.5}
+    ,{7777, "Maria", {2,2,2,2,2,1}, 18.5}
+    ,{3333, "Paulo", {0,0,2,2,2,1}, 8.7}
+    };
+    int aux[4];
+    criaIndPorNum4(array, 4,aux);
+    imprimeTumra(aux, array, 4);
+
     return 0;
 }
